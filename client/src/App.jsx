ccc9a855eccc0d5ea34f23c0298f604cbf3636aa
Adarsh Pandey <adarshpandey0873@gmail.com>
2026-04-08 14:56:52 +0530

    Update server URL to point to client deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Notes from './pages/Notes'
 import Pricing from './pages/Pricing'
 import PaymentSuccess from './pages/PaymentSuccess'
 import PaymentFailed from './pages/PaymentFailed'
-export const serverUrl = "https://examnotes-server.onrender.com"
+export const serverUrl = "https://examnotes-client.onrender.com"
 
 function App() {
   const dispatch = useDispatch()
